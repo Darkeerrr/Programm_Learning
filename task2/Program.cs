5cba249b0f6ec8876d4c7771c1b39fc733437f32
Darker <darker2045@gmail.com>
2023-09-08 20:59:29 +0300

    Сделал 2 задачи с квадратом и с гирьками

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Вес гирек: ");
+Random rnd = new Random();
+int girka1 = rnd.Next(1, 100); Console.WriteLine(girka1);
+int girka2 = rnd.Next(1, 100); Console.WriteLine(girka2);
+int girka3 = rnd.Next(1, 100); Console.WriteLine(girka3);
+int girka4 = rnd.Next(1, 100); Console.WriteLine(girka4);
+int max = girka1;
+if (girka1 > max) max = girka1;
+if (girka4 > max) max = girka4;
+if (girka3 > max) max = girka3;
+if (girka2 > max) max = girka2;
+Console.Write("Тяжелющая гирища: " + max);
