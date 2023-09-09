abb527553bdd6d94d7179dd7246aea4631478baf
Darker <darker2045@gmail.com>
2023-09-10 01:47:14 +0300

    Решил задачу с task 6 (Вывести последнее значение числа)

---
+++ task6/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу которая на вход принимает трёхзначное число а на выход выдаёт последнюю цифру этого числа
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int i = n % 10;
+Console.WriteLine(i);
