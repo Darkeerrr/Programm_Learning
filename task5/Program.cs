65f32a0ebdd2854819f10215b864a6f451cd0894
Darker <darker2045@gmail.com>
2023-09-10 01:38:36 +0300

    Решил задачу с циклом for task5

--- task5/Program.cs
+++ task5/Program.cs
@@ -1,1 +1,7 @@
-﻿// Новая задача
+﻿// Напишите программу которая вводит число и показывает промежуток от -n до n
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+for (int i = -n; i <= n; i++)
+{
+    Console.Write(i + " ");
+}
