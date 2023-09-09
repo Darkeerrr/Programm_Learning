79de95c6580db4c29524cd00d177f518af7f2693
Darker <darker2045@gmail.com>
2023-09-10 00:46:52 +0300

    Решил задачу с присваиванием номеров к дням недели task3

---
+++ task3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача номер 3. Напишите программу которая будет выдовать день недели по заданному номеру
+// Например:
+// 1 = Понедельник
+// 3 = Среда
+Console.WriteLine("Напишите номер недели: ");
+string day = Console.ReadLine();
+if (Convert.ToInt32(day) == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (Convert.ToInt32(day) == 2)
+{
+    Console.WriteLine("Понедельник");
+}
+if (Convert.ToInt32(day) == 3)
+{
+    Console.WriteLine("Понедельник");
+}
+if (Convert.ToInt32(day) == 4)
+{
+    Console.WriteLine("Понедельник");
+}
+if (Convert.ToInt32(day) == 5)
+{
+    Console.WriteLine("Понедельник");
+}
+if (Convert.ToInt32(day) == 6)
+{
+    Console.WriteLine("Понедельник");
+}
+if (Convert.ToInt32(day) == 7)
+{
+    Console.WriteLine("Понедельник");
+}
