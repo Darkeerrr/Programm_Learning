c8f0105bb594b00fd78c137050d85da26f4fba02
Darker <darker2045@gmail.com>
2023-09-10 00:57:01 +0300

    Решил задачу с сравнением числа и его квадратом task4

---
+++ task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Я вляится ли число тем из которого преобразуется квадрат этого числа
+// Например:
+// 2 = 4 true
+// 6 = 24 false
+// -3 = 9 true
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите квадрат числа: ");
+int b = int.Parse(Console.ReadLine());
+if (b == a * a)
+{
+    Console.WriteLine("Да, верно!");
+}
+else
+{
+    Console.WriteLine("Неверно!");
+}
