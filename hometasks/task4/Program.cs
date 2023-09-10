90192911fde90506d883334d69ada5f300185521
Darker <darker2045@gmail.com>
2023-09-10 19:17:46 +0300

    Добавил решение 4 домашней задачи

---
+++ hometasks/task4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа принимает на входе число N и выдаёт на выходе все чётные числа от 1 до N
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+for (int i = 1;i <= N;i++)
+{
+    int b = i%2;
+   if (b <= 0)
+   {
+    Console.Write(" " + i);
+   }
+}
