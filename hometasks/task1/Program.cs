c320d7ec12f20aefb7cff55a8279b796082c7bc2
Darker <darker2045@gmail.com>
2023-09-10 18:21:55 +0300

    Добавил папку под домашнии задания и выполнил 1 задачу в task1

---
+++ hometasks/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// На вход принимаются два числа и выводится максимальное
+Console.WriteLine("Введите первое число: ");
+int first = int.Parse(Console.ReadLine());
+Console.WriteLine("Ввелите второе число: ");
+int second = int.Parse(Console.ReadLine());
+if (first > second)
+{
+    Console.WriteLine("Первое число больше: " + first);
+}
+if (second < first)
+{
+    Console.WriteLine("Второе число больше: " + second);
+}
+if (first == second)
+{
+    Console.WriteLine("Они равны: " + first);
+}
