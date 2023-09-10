621e304c9830d66c4cf09ec9bd11199e55d539e9
Darker <darker2045@gmail.com>
2023-09-10 19:42:12 +0300

    Сложнейшая задача для Замыча

---
+++ task7/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите желаемое число: ");
+int J = int.Parse(Console.ReadLine());
+Console.WriteLine("На что делим?");
+int del = int.Parse(Console.ReadLine());
+int Res = J%del;
+Console.WriteLine("Результат: " + Res);
