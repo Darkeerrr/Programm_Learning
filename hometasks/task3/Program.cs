dfe00dcbffdc9d6fced469daaa686bf9f07f2622
Darker <darker2045@gmail.com>
2023-09-10 18:48:11 +0300

    Добавил решение домашнего задания task3

---
+++ hometasks/task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа принимает число и проверяет является ли число чётным
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+if ((a % 2) > 0)
+{
+    Console.WriteLine("Число нечётное: " + a);
+}
+else
+{
+    Console.WriteLine("Число чётное: " + a);
+}
