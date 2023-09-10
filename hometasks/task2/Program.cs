b322e953d02efc392097a75129acc833fe39646b
Darker <darker2045@gmail.com>
2023-09-10 18:37:47 +0300

    Добавил 2 задачу в домашние задания task2

---
+++ hometasks/task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Программа принимает на вход три числа и выдаёт максимальное
+Console.WriteLine("Введите первое число: ");
+int first = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int second = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int third = int.Parse(Console.ReadLine());
+if (first > second)
+{
+    if (first > third)
+    {
+        Console.WriteLine("Первое число максимальное: " + first);
+    }
+}
+if (second > first)
+{
+    if (second > third)
+    {
+        Console.WriteLine("Второе число максимальное: " + second);
+    }
+}
+if (third > first)
+{
+    if (third > second)
+    {
+        Console.WriteLine("Третье число максимальное: " + third);
+    }
+}
