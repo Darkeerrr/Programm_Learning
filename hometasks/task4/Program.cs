f03225548e034147b4ba1cb93ebc0b7f298f3af4
Darker <darker2045@gmail.com>
2023-09-14 00:51:27 +0300

    Внёс исправление в 4 домашнюю задачу с запятой

--- hometasks/task4/Program.cs
+++ hometasks/task4/Program.cs
@@ -1,11 +1,16 @@
 ﻿// Программа принимает на входе число N и выдаёт на выходе все чётные числа от 1 до N
-Console.WriteLine("Введите число: ");
-int N = int.Parse(Console.ReadLine());
-for (int i = 1;i <= N;i++)
+using System.Globalization;
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+for (int i = 2;i <= num;i++)
 {
-    int b = i%2;
-   if (b <= 0)
+   if ((i % 2 == 0 && i == num) || (i%2 == 0 && i + 1 == num))
    {
-    Console.Write(" " + i);
+    Console.Write(i);
+   }
+   else if (i % 2 == 0)
+   {
+    Console.Write(i + ",");
    }
 }
