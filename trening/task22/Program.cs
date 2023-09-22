78ea356ae6ff0bdc4eaffd5d6e5ef9a194f00cc9
Darker <darker2045@gmail.com>
2023-09-22 15:42:06 +0300

    Ещё задачки

---
+++ trening/task22/Program.cs
@@ -0,0 +1,29 @@
+﻿// задать кубический массив чисел
+int Start(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int number = Convert.ToInt32(value);
+    return number;
+}
+
+int[] Kvadrat(int number)
+{
+    int[] array = new int[number];
+    for(int i = 0; i < number; i++)
+    {
+        array[i] = (i + 1) * (i + 1) * (i + 1);
+    }
+    return array;
+}
+void Print(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int num = Start("Введите длинну массива: ");
+int[] arr = Kvadrat(num);
+Print(arr);
